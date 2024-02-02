a30dd83ab82f5f46ef5d506ad626af7a334206c2
DePacos <depacos@yandex.ru>
2024-02-02 16:54:29 +0300

    added new object 'fight' in array locations

--- script.js
+++ script.js
@@ -43,19 +43,19 @@ const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
+    // "button functions": [goStore, goCave, fightDragon],
     text: "You are in the town square. You see a sign that says \"Store\"."
   },
   {
     name: "store",
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goTown],
+    // "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
   },
   {
     name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
+    // "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
   }
 ];
@@ -63,7 +63,7 @@ const locations = [
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
-button3.onclick = fightDragon;
+// button3.onclick = fightDragon;
 
 function update(location) {
   button1.innerText = location["button text"][0];
